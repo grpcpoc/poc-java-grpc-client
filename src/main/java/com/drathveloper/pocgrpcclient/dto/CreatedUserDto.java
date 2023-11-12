6aeca22dfe6a591484f498cb4d4d0d954f2e82fb
drathveloper <alberto.hdzhdz@gmail.com>
2023-11-12 12:00:31 +0100

    initial commit

--- src/main/java/com/drathveloper/pocgrpcclient/dto/CreatedUserDto.java
+++ src/main/java/com/drathveloper/pocgrpcclient/dto/CreatedUserDto.java
@@ -1,4 +1,7 @@
 package com.drathveloper.pocgrpcclient.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record CreatedUserDto(Long id, String username) {
 }
